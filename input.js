6d4f0dd2ccda22b1ec515f65156ed4237ee9f3a2
loganlennox <loganlennox@protonmail.com>
2018-07-02 18:58:53 -0400

    Detect explicit words in user messages

--- input.js
+++ input.js
@@ -1,12 +1,26 @@
+const censor = require("./res/censor.json");
 
 exports.routeInput = (msg) => {
-  console.log(`Routing... "${msg}"`);
-
-  // 1 - Check if message is explicit
+ if (exports.isExplicit(msg)) {
+   // Delete message
+   return;
+ }
 
   // 2 - Check if message is command
 
   // 3 - Check if message is trigger
 
   console.log(`"${msg}" is a normal message.`);
+};
+
+exports.isExplicit = (input) => {
+  for (let w of censor.badwords) {
+    let re = new RegExp(w, "gi");
+
+    if (re.exec(input) != null) {
+      return true;
+    }
+  }
+
+  return false;
 };
