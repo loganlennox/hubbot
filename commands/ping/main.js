c973b86c21b891206c183f16b5753934f87cebd6
loganlennox <loganlennox@protonmail.com>
2018-07-06 09:45:52 -0400

    Simplify ping command

--- commands/ping/main.js
+++ commands/ping/main.js
@@ -1,7 +1,5 @@
 const util = include("/utilities.js");
 
 exports.execute = (input, args) => {
-  let time = Math.round((Date.now() - input.createdTimestamp) / 1000);
-
-  util.send_msg(input.channel.id, `Pong! Took me ${time}ms to respond!`);
+  util.send_msg(input.channel.id, `Pong! Took me ${util.client.ping}ms to respond!`);
 };
