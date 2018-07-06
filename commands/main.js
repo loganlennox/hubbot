956713332d13624fa1ed938e8645fbeaa7779f5a
loganlennox <loganlennox@protonmail.com>
2018-07-06 03:50:22 -0400

    Preserve letter casing of command arguments

--- commands/main.js
+++ commands/main.js
@@ -4,12 +4,12 @@ const config = include("/config.json");
 
 // if the given input is a command, attempt to execute it
 exports.handleCommand = (input) => {
-  const cmd = input.toString().toLowerCase().substring(1).split(" ");
+  const cmd = input.toString().substring(1).split(" ");
 
-  if (!fs.readdirSync(abs_path("/commands/")).filter(f => f == cmd[0]).length > 0)
+  if (!fs.readdirSync(abs_path("/commands/")).filter(f => f == cmd[0].toLowerCase()).length > 0)
     return false;
 
-  const command = include(`/commands/${cmd[0]}/main.js`);
+  const command = include(`/commands/${cmd[0].toLowerCase()}/main.js`);
 
   command.execute(input, cmd);
 
