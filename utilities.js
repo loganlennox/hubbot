1368a240bcfbe00ba66982d5591b4a4359303b77
loganlennox <loganlennox@protonmail.com>
2018-07-09 15:09:44 -0400

    Create utility function to format a Date object

--- utilities.js
+++ utilities.js
@@ -6,6 +6,18 @@ const config = include("/config.json")
 
 exports.client = null;
 
+exports.format_date = (date) => {
+  const year = date.getFullYear();
+
+  let month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : "0" + month;
+
+  let day = date.getDate().toString();
+  day = day.length > 1 ? day : "0" + day;
+
+  return `${month} / ${day} / ${year}`;
+};
+
 exports.send_raw_msg = (channelID, message, time = 0) => {
   exports.client.channels.get(channelID).send(message)
     .then((m) => (time > 0) ? m.delete(time) : null)
